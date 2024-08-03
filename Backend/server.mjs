86b1d988cd97adbe5af490eb9c4a90e03c3fc1a4
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-08-03 11:40:24 +0530

    chore: Update server host to use localhost

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -13,7 +13,7 @@ app.use(cors());
 // PostgreSQL pool setup
 const pool = new Pool({
   user: 'postgres',
-  host: '0.0.0.0',
+  host: 'localhost',
   database: 'EMS',
   password: 'root',
   port: 5432,
